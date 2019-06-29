574a24745f05193cb7339e207a5f2d28e3440ccb
Anton <Dobriskazo4nik@mail.ru>
2019-06-29 17:48:46 +0300

    Min or Max Number

--- devolopment.py
+++ devolopment.py
@@ -1,3 +1,15 @@
+# Max or min number
 from random import randrange
 rand_list = [randrange(20) for i in range(5)]
+num_min = rand_list[0]
+num_max = rand_list[0]
+
 print(rand_list)
+for i in range(len(rand_list)):
+	if num_min > rand_list[i]:
+		num_min = rand_list[i]
+	elif num_max < rand_list[i]:
+		num_max = rand_list[i]
+
+
+print ("Min: ",num_min,"\n","Max: ",num_max)
