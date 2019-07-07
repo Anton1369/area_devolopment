a31ef0e0c4e5d1a7e21373ae5a4d449f35c4a495
Anton <Dobriskazo4nik@mail.ru>
2019-07-07 13:24:47 +0300

    even or odd

--- func.py
+++ func.py
@@ -3,3 +3,9 @@ def empty_fun ():# empty function
 
 def sqrt_func(num):#"return sqrt number"
 	return num**2
+
+def even_odd(num):#even or odd
+	if num % 2 == 0:
+		print('yes')
+	else:
+		print('no')
