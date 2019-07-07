76454eab82b9142ea2427737d6e9b875b5824633
Anton <Dobriskazo4nik@mail.ru>
2019-07-07 14:37:42 +0300

    Taski

--- func.py
+++ func.py
@@ -31,3 +31,52 @@ def decorator_function(func):
 def hello_world():
      print('Hello world!')
 print(hello_world())
+
+
+list_number = list(map(lambda x: x**2, [i for i in range(10)]))
+print(list_number)
+
+list_word = list(filter(lambda x: x % 2 == 0, [i for i in range(20)]))
+print(list_word)
+
+l = []
+x = 2
+def clean(s):#clean function
+	s += 1
+	return s
+print(clean(x))
+print(x)
+
+def dirty(l):#dirty function
+	return l.append(2)
+print(l)
+
+def min_max (arg):#min max
+	x_min = None
+	x_max = None
+	for i in arg:
+		if x_min:
+			if x_min > i:
+				x_min = i
+		else:
+			x_min = i
+
+	for n in arg:
+		if x_max:
+			if x_max < n:
+				x_max = n
+		else:
+			x_max = n
+	return x_min , x_max
+print(min_max([1,2,3,4,20]))
+
+def year(number):
+	return (True if number % 4 == 0 and (number % 100 == 0  and number % 400 != 0) else False)
+print(year(2000))
+
+def season (month):
+	d = {'Зима':[12,1,2],'Весна':[3,4,5],'Лето':[6,7,8],'Осень':[9,10,11]}
+	for k,v in d.items():
+		if month in v:
+			return k
+print(season (12))
